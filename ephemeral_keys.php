d080e920d13588dc5a1f1d88acd2db1005d87042
Hung Hoang <hunghbd.pnv@gmail.com>
2017-12-11 16:38:48 +0700

    no message

--- ephemeral_keys.php
+++ ephemeral_keys.php
@@ -8,7 +8,7 @@ require_once('vendor/autoload.php');
 // This assumes that $customerId has been set appropriately from session data
 if (!isset($_POST['api_version']))
 {
-    exit(http_response_code(400));
+    echo(http_response_code(400));
 }
 try {
     $key = \Stripe\EphemeralKey::create(
@@ -16,9 +16,9 @@ try {
       array("stripe_version" => $_POST['api_version'])
     );
     header('Content-Type: application/json');
-    exit(json_encode($key));
+    echo(json_encode($key));
 } catch (Exception $e) {
-    exit(http_response_code(500));
+    echo(http_response_code(500));
 }
 
 ?>
