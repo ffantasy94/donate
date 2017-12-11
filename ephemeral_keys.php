c0c197353bfb36948ea3ae412629b7c185bd83fc
Hung Hoang <hunghbd.pnv@gmail.com>
2017-12-11 17:00:28 +0700

    no message

--- ephemeral_keys.php
+++ ephemeral_keys.php
@@ -5,21 +5,45 @@ require_once('vendor/autoload.php');
 // See your keys here https://dashboard.stripe.com/account/apikeys
 \Stripe\Stripe::setApiKey(getenv('STRIPE_TEST_SECRET_KEY'));
 
+function json_response($message = null, $code = 200)
+{
+    // clear the old headers
+    header_remove();
+    // set the actual code
+    http_response_code($code);
+    // set the header to make sure cache is forced
+    header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+    // treat this as json
+    header('Content-Type: application/json');
+    $status = array(
+        200 => '200 OK',
+        400 => '400 Bad Request',
+        422 => 'Unprocessable Entity',
+        500 => '500 Internal Server Error'
+        );
+    // ok, validation error, or failure
+    header('Status: '.$status[$code]);
+    // return the encoded json
+    return json_encode(array(
+        'status' => $code < 300, // success or not?
+        'message' => $message
+        ));
+}
+
 // This assumes that $customerId has been set appropriately from session data
 if (!isset($_POST['api_version']))
 {
-    echo(http_response_code(400));
+    exit(json_response(400, 'error')); // {"status":true,"message":"working"}
 }
+
 try {
     $key = \Stripe\EphemeralKey::create(
       array("customer" => $_POST['customer_id']),
       array("stripe_version" => $_POST['api_version'])
     );
-    header("HTTP/1.1 200 OK");
-    header('Content-Type: application/json');
-    echo(json_encode($key));
+    exit(json_response(200, $key)); // {"status":true,"message":"working"}
 } catch (Exception $e) {
-    echo(http_response_code(500));
+    exit(json_response(500, 'error')); // {"status":true,"message":"working"}
 }
 
 ?>
